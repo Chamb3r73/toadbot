3c2dad0d14d9e77b37c08f11ebd1ac816fccce2c
Chamb3r73 <59922982+Chamb3r73@users.noreply.github.com>
2020-08-10 21:57:38 +0100

    update index.js
    updated to include link to config file which will be uploaded next

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const config = require('./config.json');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -5,9 +6,11 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
-if (message.content === '!ping') {
-	// send back "Pong." to the channel the message was sent in
-	message.channel.send('Pong.');
-}
+client.on('message', message => {
+	if (message.content === 't!ping') {
+		// send back "Pong." to the channel the message was sent in
+		message.channel.send('Pong.');
+	}
+});
 
-client.login('NzIzMTcxODYzODg2Mjk5MTU2.XutwSA.H8UPt8LvFUOqIymJhmUoNHKxjFA');
+client.login('token-goes-here');
