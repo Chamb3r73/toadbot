aaf479cb80d3586cba79d52f311db54cec577bfa
Chamb3r73 <59922982+Chamb3r73@users.noreply.github.com>
2020-08-11 10:55:14 +0100

    index.js update
    toad-embed as a test for embeds

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const config = require('./config.json');
+const { prefix, token } = require('./config.json');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -6,11 +6,31 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.on('message', message => {
-	if (message.content === 't!ping') {
-		// send back "Pong." to the channel the message was sent in
+client.on('message', message => {if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+	if (message.content.startsWith(`${prefix}ping`)) {
 		message.channel.send('Pong.');
+	
+	} else if (message.content === `${prefix}server`) {
+		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+	} else if (message.content === `${prefix}toad-embed`) {
+			const exampleEmbed = new Discord.MessageEmbed()
+				.setColor('#F47FFF')
+				.setTitle('Toad Embed')
+				.setAuthor('toadbot')
+				.setDescription('badda bing badda bong, here\'s an embed with toad pictures')
+				.attachFiles(['../discordbot/assets/toadembed.jpg'])
+				.setImage('attachment://toadembed.jpg')
+				.setThumbnail('attachment://toadembed.jpg')
+		
+			message.channel.send(exampleEmbed);
+		
 	}
+	
+	
+
 });
 
-client.login('token-goes-here');
+client.login(token);
