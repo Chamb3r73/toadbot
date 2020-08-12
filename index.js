ddd856f3942b1d39dc04216d0ae61a6190d80ef8
Chamb3r73 <59922982+Chamb3r73@users.noreply.github.com>
2020-08-12 19:19:59 +0100

    Update index.js
    added the finished t!toad command

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { prefix, token } = require('./config.json');
+const fetch = require('node-fetch');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -6,7 +7,8 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.on('message', message => {if (!message.content.startsWith(prefix) || message.author.bot) return;
+client.on('message', async message => 
+	{if (!message.content.startsWith(prefix) || message.author.bot) return;
 
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
 	const command = args.shift().toLowerCase();
@@ -15,18 +17,42 @@ client.on('message', message => {if (!message.content.startsWith(prefix) || mess
 	
 	} else if (message.content === `${prefix}server`) {
 		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
-	} else if (message.content === `${prefix}toad-embed`) {
-			const exampleEmbed = new Discord.MessageEmbed()
-				.setColor('#F47FFF')
-				.setTitle('Toad Embed')
-				.setAuthor('toadbot')
-				.setDescription('badda bing badda bong, here\'s an embed with toad pictures')
-				.attachFiles(['../discordbot/assets/toadembed.jpg'])
-				.setImage('attachment://toadembed.jpg')
-				.setThumbnail('attachment://toadembed.jpg')
-		
-			message.channel.send(exampleEmbed);
-		
+	} else if (message.content === `${prefix}toad`) {
+		var toadRandom = Math.floor(Math.random() *5);
+		console.log(toadRandom);
+		if (toadRandom === 0) {
+			const toad1 = new Discord.MessageAttachment('../discordbot/assets/toad1.jpg')
+			message.channel.send(toad1)
+		} else if (toadRandom === 1) {
+			const toad2 = new Discord.MessageAttachment('../discordbot/assets/toad2.jpg')
+			message.channel.send(toad2)
+		} else if (toadRandom === 2) {
+			const toad3 = new Discord.MessageAttachment('../discordbot/assets/toad3.jpg')
+			message.channel.send(toad3)
+		} else if (toadRandom === 3) {
+			const toad4 = new Discord.MessageAttachment('../discordbot/assets/toad4.jpg')
+			message.channel.send(toad4)
+		} else if (toadRandom === 4) {
+			const toad5 = new Discord.MessageAttachment('../discordbot/assets/toad5.jpg')
+			message.channel.send(toad5)
+		} else if (toadRandom === 5) {
+			const toad6 = new Discord.MessageAttachment('../discordbot/assets/toad6.jpg')
+			message.channel.send(toad6)
+		}
+
+	} else if (message.content === `${prefix}react`) {
+		message.react('742684152176050227');
+	} else if (message.content === `${prefix}bot`) {
+		const botEmbed = new Discord.MessageEmbed()
+			.setColor('#F47FFF')
+			.setTitle('Bot Status')
+			.setDescription('Active and Online')
+			.attachFiles(['../discordbot/assets/linusface.jpg'])
+			.setThumbnail('attachment://linusface.jpg')
+		message.channel.send(botEmbed);
+    
+	} else if (message.content === `${prefix}bobberto`) {
+		message.channel.send('bobberto? more like lamerto')
 	}
 	
 	
@@ -34,3 +60,5 @@ client.on('message', message => {if (!message.content.startsWith(prefix) || mess
 });
 
 client.login(token);
+
+
