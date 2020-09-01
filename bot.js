0f569e3fe8da4f2b9a998c82e3e730653157367a
Chamb3r73 <59922982+Chamb3r73@users.noreply.github.com>
2020-09-01 22:07:04 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,30 +1,25 @@
-// const { prefix, token } = require('./config.json');
-// const fetch = require('node-fetch');
 const Discord = require('discord.js');
+
 const client = new Discord.Client();
-// var PORT = process.env.PORT || 5000;
-var prefix = "t!";
 
+ 
 
-client.once('ready', () => {
-	console.log('Ready!');
-	console.log('balls')
+client.on('ready', () => {
+
+    console.log('I am ready!');
+
 });
 
-client.on('message', async message => 
-	
-	{if (!message.content.startsWith(prefix) || message.author.bot) return;
+ 
 
-	const args = message.content.slice(prefix.length).trim().split(/ +/);
-	const command = args.shift().toLowerCase();
-	var prefix = "t!";
-	if (message.content.startsWith(`${prefix}test`)) {
-		message.channel.send('it finally works thank god');
-	
-	} else if (message.content === 'ping') {
-		message.reply('pong');
+client.on('message', message => {
+
+    if (message.content === 'ping') {
+
+       message.reply('pong');
 
        }
+
 });
 
-client.login(process.env.BOT_TOKEN);
+ 
