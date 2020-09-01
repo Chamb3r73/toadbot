a3b17382e9590d24d016de4882930603eb6df150
Chamb3r73 <59922982+Chamb3r73@users.noreply.github.com>
2020-09-01 22:22:11 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,8 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
+var prefix = 't!';
+
  
 
 client.on('ready', () => {
@@ -13,14 +15,23 @@ client.on('ready', () => {
  
 
 client.on('message', message => {
+    {if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    if (message.content === 'ping') {
+        const args = message.content.slice(prefix.length).trim().split(/ +/);
+        const command = args.shift().toLowerCase();
 
-       message.reply('pong');
+            if (message.content === 'ping') {
 
-       } else if (message.content === 'test') {
-           message.reply('test success')
-       }
+            message.reply('pong');
+
+            } else if (message.content === 'test') {
+                message.reply('test success')
+            } else if (command === test2) {
+                message.reply('test2 success')
+            }
+
+        
+    }      
 
 });
 
