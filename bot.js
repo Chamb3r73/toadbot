b17e384cedc73f45ec73b484df3f680336229c73
Chamb3r73 <59922982+Chamb3r73@users.noreply.github.com>
2020-09-02 10:10:03 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,134 +1,123 @@
+// const { prefix, token } = require('./config.json');
+// const fetch = require('node-fetch');
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
+// var PORT = process.env.PORT || 5000;
+var prefix = ("t!")
 
-var prefix = 't!';
 
- 
-
-client.on('ready', () => {
-
-    console.log('I am ready!');
-
+client.once('ready', () => {
+	console.log('om bot online and ready');
 });
 
- 
-
-client.on('message', message => {
-    {if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-        const args = message.content.slice(prefix.length).trim().split(/ +/);
-        const command = args.shift().toLowerCase();
-
-            if (message.content === 'ping') {
-
-            message.reply('pong');
-
-            } else if (message.content === 'test') {
-                message.reply('test success')
-            } else if (command === 'test2') {
-                message.reply('test2 success')
-            } if (command === 'ping') {
-	               message.channel.send('Pong.');
+client.on('message', async message => 
 	
-	           } else if (command === 'server') {
-		              message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+	{if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-	           } else if (command === 'toad') {
-		              var toadRandom = Math.floor(Math.random() *5);
-		              console.log(toadRandom);
-		              if (toadRandom === 0) {
-                    const toadEmbed1 = new Discord.MessageEmbed()
-                        .setColor('#F47FFF')
-                        .setTitle('who doesnt love a good toad')
-                        .attachFiles(['../discordbot/assets/toad1.jpg'])
-                        .setImage('attachment://toad1.jpg')
-                    message.channel.send(toadEmbed1)
-                 } else if (toadRandom === 1) {
-                    const toadEmbed2 = new Discord.MessageEmbed()
-                        .setColor('#F47FFF')
-                        .setTitle('who doesnt love a good toad')
-                        .attachFiles(['../discordbot/assets/toad2.jpg'])
-                        .setImage('attachment://toad2.jpg')
-                    message.channel.send(toadEmbed2)
-                 } else if (toadRandom === 2) {
-                    const toadEmbed3 = new Discord.MessageEmbed()
-                        .setColor('#F47FFF')
-                        .setTitle('who doesnt love a good toad')
-                        .attachFiles(['../discordbot/assets/toad3.jpg'])
-                        .setImage('attachment://toad3.jpg')
-                    message.channel.send(toadEmbed2)
-                 } else if (toadRandom === 3) {
-                    const toadEmbed4 = new Discord.MessageEmbed()
-                        .setColor('#F47FFF')
-                        .setTitle('who doesnt love a good toad')
-                        .attachFiles(['../discordbot/assets/toad4.jpg'])
-                        .setImage('attachment://toad4.jpg')
-                    message.channel.send(toadEmbed4)
-                 } else if (toadRandom === 4) {
-                    const toadEmbed5 = new Discord.MessageEmbed()
-                        .setColor('#F47FFF')
-                        .setTitle('who doesnt love a good toad')
-                        .attachFiles(['../discordbot/assets/toad5.jpg'])
-                        .setImage('attachment://toad5.jpg')
-                    message.channel.send(toadEmbed5)
-                 } else if (toadRandom === 5) {
-                    const toadEmbed6 = new Discord.MessageEmbed()
-                        .setColor('#F47FFF')
-                        .setTitle('who doesnt love a good toad')
-                        .attachFiles(['../discordbot/assets/toad6.jpg'])
-                        .setImage('attachment://toad6.jpg')
-                    message.channel.send(toadEmbed6)
-                 } else if (toadRandom === 6) {
-                    const toadEmbed7 = new Discord.MessageEmbed()
-                        .setColor('#F47FFF')
-                        .setTitle('who doesnt love a good toad')
-                        .attachFiles(['../discordbot/assets/toad7.jpg'])
-                        .setImage('attachment://toad7.jpg')
-                    message.channel.send(toadEmbed7)
-                   }
-             
-             } else if (command === 'bot') {
-                 const botEmbed = new Discord.MessageEmbed()
-                   .setColor('#F47FFF')
-                   .setTitle('Bot Status')
-                   .setDescription('Active and Online')
-                   .attachFiles(['../discordbot/assets/linusface.jpg'])
-                   .setThumbnail('attachment://linusface.jpg')
-                   .setFooter('i was made by @toad#9999')
-                 message.channel.send(botEmbed);
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+	if (command === 'ping') {
+		message.channel.send('Pong.');
+	
+	} else if (command === 'server') {
+		const serverEmbed = new Discord.MessageEmbed()
+			.setColor('#F47FFF')
+			.setTitle('Server Stats')
+			.setDescription(`Server name: ${message.guild.name}\nTotak members: ${message.guild.membercount}`)
+	
+
+	} else if (command === 'toad') {
+		var toadRandom = Math.floor(Math.random() *5);
+		console.log(toadRandom);
+		if (toadRandom === 0) {
+			const toadEmbed1 = new Discord.MessageEmbed()
+                .setColor('#F47FFF')
+			    .setTitle('who doesnt love a good toad')
+			    .attachFiles(['../discordbot/assets/toad1.jpg'])
+			    .setImage('attachment://toad1.jpg')
+			message.channel.send(toadEmbed1)
+		} else if (toadRandom === 1) {
+			const toadEmbed2 = new Discord.MessageEmbed()
+                .setColor('#F47FFF')
+			    .setTitle('who doesnt love a good toad')
+			    .attachFiles(['../discordbot/assets/toad2.jpg'])
+			    .setImage('attachment://toad2.jpg')
+			message.channel.send(toadEmbed2)
+		} else if (toadRandom === 2) {
+			const toadEmbed3 = new Discord.MessageEmbed()
+                .setColor('#F47FFF')
+			    .setTitle('who doesnt love a good toad')
+			    .attachFiles(['../discordbot/assets/toad3.jpg'])
+			    .setImage('attachment://toad3.jpg')
+			message.channel.send(toadEmbed2)
+		} else if (toadRandom === 3) {
+			const toadEmbed4 = new Discord.MessageEmbed()
+                .setColor('#F47FFF')
+			    .setTitle('who doesnt love a good toad')
+			    .attachFiles(['../discordbot/assets/toad4.jpg'])
+			    .setImage('attachment://toad4.jpg')
+			message.channel.send(toadEmbed4)
+		} else if (toadRandom === 4) {
+			const toadEmbed5 = new Discord.MessageEmbed()
+                .setColor('#F47FFF')
+			    .setTitle('who doesnt love a good toad')
+			    .attachFiles(['../discordbot/assets/toad5.jpg'])
+			    .setImage('attachment://toad5.jpg')
+			message.channel.send(toadEmbed5)
+		} else if (toadRandom === 5) {
+			const toadEmbed6 = new Discord.MessageEmbed()
+                .setColor('#F47FFF')
+			    .setTitle('who doesnt love a good toad')
+			    .attachFiles(['../discordbot/assets/toad6.jpg'])
+			    .setImage('attachment://toad6.jpg')
+			message.channel.send(toadEmbed6)
+		} else if (toadRandom === 6) {
+			const toadEmbed7 = new Discord.MessageEmbed()
+                .setColor('#F47FFF')
+			    .setTitle('who doesnt love a good toad')
+			    .attachFiles(['../discordbot/assets/toad7.jpg'])
+			    .setImage('attachment://toad7.jpg')
+			message.channel.send(toadEmbed7)
+		}
+
+	} else if (command === 'bot') {
+		const botEmbed = new Discord.MessageEmbed()
+			.setColor('#F47FFF')
+			.setTitle('Bot Status')
+			.setDescription('Active and Online')
+			.attachFiles(['../discordbot/assets/linusface.jpg'])
+			.setThumbnail('attachment://linusface.jpg')
+			.setFooter('i was made by @toad#9999')
+		message.channel.send(botEmbed);
     
-             } else if (command === 'bobberto') {
-                 message.channel.send('bobberto? more like lamerto')
+	} else if (command === 'bobberto') {
+		message.channel.send('bobberto? more like lamerto')
 
-             } else if (command === 'help') {
-                 const helpEmbed = new Discord.MessageEmbed()
-                   .setColor('#F47FFF')
-                   .setTitle('toadbot Commands')
-                   .setDescription('t!bot - bot status \n !toad - random toad pic (dm me more) \n t!bot - bot status \n t!server - server info \n t!bobberto - epik funny \n t!say - bot repeats what you said and deletes your message')
-                   .attachFiles(['../discordbot/assets/linusface.jpg'])
-                   .setThumbnail('attachment://linusface.jpg')
-                   .setFooter('i was made by @toad#9999') 
-                 message.channel.send(helpEmbed)
-
-
-             } else if (command === 'cruise') {
-                message.channel.send('toadbot is a fan of tom cruise confirmed!!!11!!!!!')
-             } else if (command === 'say') {
-                if (!args.length) {
-                 return message.channel.send(`say something smh ${message.author}!`);
-                }
-
-                  message.channel.send(`${args.join(' ')}`);
-                  message.delete(args)
-               }
+	} else if (command === 'help') {
+		const helpEmbed = new Discord.MessageEmbed()
+			.setColor('#F47FFF')
+			.setTitle('toadbot Commands')
+			.setDescription('t!bot - bot status \n !toad - random toad pic (dm me more) \n t!bot - bot status \n t!server - server info \n t!bobberto - epik funny \n t!say - bot repeats what you said and deletes your message')
+			.attachFiles(['../linusface.jpg'])
+			.setThumbnail('attachment://linusface.jpg')
+			.setFooter('i was made by @toad#9999') 
+		message.channel.send(helpEmbed)
 
 
-         }      
+	} else if (command === 'cruise') {
+		message.channel.send('toadbot is a fan of tom cruise confirmed!!!11!!!!!')
+	} else if (command === 'say') {
+		if (!args.length) {
+			return message.channel.send(`say something smh ${message.author}!`);
+		}
+	
+		message.channel.send(`${args.join(' ')}`);
+		message.delete(args)
+	}
+
+
+
 
 });
-
- 
-
 
 client.login(process.env.BOT_TOKEN);
