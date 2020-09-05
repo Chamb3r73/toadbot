d65d47784f134b27320eb09a5a55818fb1d85073
Chamb3r73 <59922982+Chamb3r73@users.noreply.github.com>
2020-09-05 21:34:44 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -108,11 +108,10 @@ client.on('message', async message =>
 	} else if (command === 'cruise') {
 		message.channel.send('toadbot is a fan of tom cruise confirmed!!!11!!!!!')
 	} else if (command === 'say') {
-		if (!message.member.hasPermission("MENTION_EVERYONE")) return message.channel.send('invalid perms get rekt (dont ping @ everyone you fucking tool')
 		if (!args.length) {
 			return message.channel.send(`say something smh ${message.author}!`);
 		}
-	
+		if (!message.member.hasPermission("MENTION_EVERYONE")) return message.channel.send('invalid perms get rekt (dont ping @ everyone you fucking tool')
 		message.channel.send(`${args.join(' ')}`);
 		message.delete(args)
 	} else if (command === 'pog') {
