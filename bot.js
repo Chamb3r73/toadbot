810e4e4f42df4b67f5a1f712a03690131c5c7a67
Chamb3r73 <59922982+Chamb3r73@users.noreply.github.com>
2020-09-28 21:37:01 +0100

    t!help cleaning changes #1

--- bot.js
+++ bot.js
@@ -98,7 +98,7 @@ client.on('message', async message =>
 		const helpEmbed = new Discord.MessageEmbed()
 			.setColor('#F47FFF')
 			.setTitle('toadbot Commands')
-			.setDescription('t!bot - bot status \n !toad - random toad pic (dm me more) \n t!server - server info \n t!bobberto - epik funny \n t!say - bot repeats what you said and deletes your message \n t!help - list of commands \n t!pog - poggers \n kick - kicks @ed user \n t!ban - bans @ed user \n t!mute - mutes @ed user [IN PROGRESS]')
+		.setDescription('t!bot - bot status \n t!toad - random toad pic (dm me more) \n t!server - server info \n t!bobberto - epik funny \n t!say - bot repeats what you said and deletes your message \n t!help - list of commands \n t!pog - poggers \n t!kick - kicks @ed user \n t!ban - bans @ed user \n t!mute - mutes @ed user [IN PROGRESS] \n t!invite - invite link just for you buddy')
 			.attachFiles(['linusface.jpg'])
 			.setThumbnail('attachment://linusface.jpg')
 			.setFooter('i was made by @toad#9999') 
