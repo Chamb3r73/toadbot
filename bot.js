3fc4ba0c57ffa87ab1cefd3ae8801ab1f7e0ec72
Amal kuriakose <67690738+nolifeasian24-7@users.noreply.github.com>
2020-09-28 18:13:50 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,182 @@
+// const { prefix, token } = require('./config.json');
+// const fetch = require('node-fetch');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+// var PORT = process.env.PORT || 5000;
+var prefix = ("t!")
+
+
+client.once('ready', () => {
+	console.log('om bot online and ready');
+});
+
+client.on('message', async message => 
+	
+	{if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+	if (command === 'ping') {
+		message.channel.send('Pong.');
+	
+	} else if (command === 'server') {
+		const serverEmbed = new Discord.MessageEmbed()
+			.setColor('#F47FFF')
+			.setTitle('Server Stats')
+			.setDescription(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}\nOwner: ${message.guild.owner}`)
+		message.channel.send(serverEmbed)
+	
+
+	} else if (command === 'toad') {
+		var toadRandom = Math.floor(Math.random() *5);
+		console.log(toadRandom);
+		if (toadRandom === 0) {
+			const toadEmbed1 = new Discord.MessageEmbed()
+                .setColor('#F47FFF')
+			    .setTitle('who doesnt love a good toad')
+			    .attachFiles(['toad/toad1.jpg'])
+			    .setImage('attachment://toad1.jpg')
+			message.channel.send(toadEmbed1)
+		} else if (toadRandom === 1) {
+			const toadEmbed2 = new Discord.MessageEmbed()
+                .setColor('#F47FFF')
+			    .setTitle('who doesnt love a good toad')
+			    .attachFiles(['toad/toad2.jpg'])
+			    .setImage('attachment://toad2.jpg')
+			message.channel.send(toadEmbed2)
+		} else if (toadRandom === 2) {
+			const toadEmbed3 = new Discord.MessageEmbed()
+                .setColor('#F47FFF')
+			    .setTitle('who doesnt love a good toad')
+			    .attachFiles(['toad/toad3.jpg'])
+			    .setImage('attachment://toad3.jpg')
+			message.channel.send(toadEmbed2)
+		} else if (toadRandom === 3) {
+			const toadEmbed4 = new Discord.MessageEmbed()
+                .setColor('#F47FFF')
+			    .setTitle('who doesnt love a good toad')
+			    .attachFiles(['toad/toad4.jpg'])
+			    .setImage('attachment://toad4.jpg')
+			message.channel.send(toadEmbed4)
+		} else if (toadRandom === 4) {
+			const toadEmbed5 = new Discord.MessageEmbed()
+                .setColor('#F47FFF')
+			    .setTitle('who doesnt love a good toad')
+			    .attachFiles(['toad/toad5.jpg'])
+			    .setImage('attachment://toad5.jpg')
+			message.channel.send(toadEmbed5)
+		} else if (toadRandom === 5) {
+			const toadEmbed6 = new Discord.MessageEmbed()
+                .setColor('#F47FFF')
+			    .setTitle('who doesnt love a good toad')
+			    .attachFiles(['toad/toad6.jpg'])
+			    .setImage('attachment://toad6.jpg')
+			message.channel.send(toadEmbed6)
+		} else if (toadRandom === 6) {
+			const toadEmbed7 = new Discord.MessageEmbed()
+                .setColor('#F47FFF')
+			    .setTitle('who doesnt love a good toad')
+			    .attachFiles(['toad/toad7.jpg'])
+			    .setImage('attachment://toad7.jpg')
+			message.channel.send(toadEmbed7)
+		}
+
+	} else if (command === 'bot') {
+		const botEmbed = new Discord.MessageEmbed()
+			.setColor('#F47FFF')
+			.setTitle('Bot Status')
+			.setDescription('Active and Online')
+			.attachFiles(['linusface.jpg'])
+			.setThumbnail('attachment://linusface.jpg')
+			.setFooter('i was made by @toad#9999')
+		message.channel.send(botEmbed);
+    
+	} else if (command === 'bobberto') {
+		message.channel.send('bobberto? more like lamerto')
+
+	} else if (command === 'help') {
+		const helpEmbed = new Discord.MessageEmbed()
+			.setColor('#F47FFF')
+			.setTitle('toadbot Commands')
+			.addFields(
+				{
+					name: "moderation:",
+					value: "1. kick (Kicks the @ user)\n 2. ban (bans the @ user) \n 3. Mute (Mutes the @ user, in progress)",
+					inline: false
+				},
+
+				{
+					name: "entertainment:",
+					value: "1.bobberto (big funny) \n 2. say (repeats what user said and deletes the message) \n 3. Pog (poggers) \n 4. cruise (toadbot is fan of Tom cruise om!!!)",
+					inline: false
+				},
+
+				{
+					name: "info",
+					value: "1.server: (returns server info) \n 2. invite (doitdoitdoitdoitdoitdoit)",
+					inline: false
+				}
+			)
+			.attachFiles(['linusface.jpg'])
+			.setThumbnail('attachment://linusface.jpg')
+			.setFooter('i was made by @toad#9999') 
+		message.channel.send(helpEmbed)
+
+
+	} else if (command === 'cruise') {
+		message.channel.send('toadbot is a fan of tom cruise confirmed!!!11!!!!!')
+		
+		
+	} else if (command === 'say') {
+		if (!args.length) {
+			return message.channel.send(`say something smh ${message.author}!`);
+		}
+		if (message.content.includes('@everyone')) {
+			if (!message.member.hasPermission("MENTION_EVERYONE")) return message.channel.send('invalid perms get rekt')
+		}
+		else if (message.content.includes('@here')) {
+			if (!message.member.hasPermission("MENTION_EVERYONE")) return message.channel.send('invalid perms get rekt')
+		}
+		else {
+			message.channel.send(`${args.join(' ')}`);
+			message.delete(args)
+		}
+		
+		
+	} else if (command === 'pog') {
+		const pogEmbed = new Discord.MessageEmbed()
+			.attachFiles(['pog.gif'])
+			.setImage('attachment://pog.gif')
+			.setColor('#F47FFF')
+		message.channel.send(pogEmbed)
+
+	} else if (command === 'kick') {
+		if (!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send('invalid perms get rekt')
+		let User = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
+		if (!User) return message.channel.send("Invalid User")
+		if (User.hasPermission("KICK_MEMBERS")) return message.reply("Invalid Permissions")
+		let kickReason = args.join(" ").slice(22);
+		if (!kickReason) {
+			Reason = "None"
+		}
+		User.kick({reason: kickReason})	
+
+	} else if (command === 'ban') {
+		if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send('invalid perms get rekt')
+		let User = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
+		if (!User) return message.channel.send("Invalid User")
+		if (User.hasPermission("BAN_MEMBERS")) return message.reply("Invalid Permissions")
+		let banReason = args.join(" ").slice(22);
+		if (!banReason) {
+			Reason = "None"
+		}
+		User.kick({reason: banReason})
+		
+	}
+
+
+
+
+});
+
+client.login(process.env.BOT_TOKEN);
