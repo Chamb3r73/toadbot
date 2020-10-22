efacd3c7167c2feca65ebb18539eb69705fd7833
Chamb3r73 <59922982+Chamb3r73@users.noreply.github.com>
2020-10-22 12:55:20 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -95,7 +95,7 @@ client.on('message', async message =>
 		const botEmbed = new Discord.MessageEmbed()
 			.setColor('#F47FFF')
 			.setTitle('Bot Status')
-			.setDescription('Active and Online')
+			.setDescription(`Active and Online\n The bot is in: ${client.guilds}`)
 			.attachFiles(['linusface.jpg'])
 			.setThumbnail('attachment://linusface.jpg')
 			.setFooter('i was made by @toad#9999')
