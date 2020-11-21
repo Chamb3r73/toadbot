cf93130eb45672d9101d66e80ba21c4a2404c7e7
Chamb3r73 <59922982+Chamb3r73@users.noreply.github.com>
2020-11-21 20:53:28 +0000

    fix

--- commands.js
+++ commands.js
@@ -1,13 +1,3 @@
-const Discord = require('discord.js');
 const testVar = "hi tober";
 module.exports = { testVar };
-
-module.exports = {
-
-    name: 'testoFUNC',
-    description: 'crikey!',
-    execute(message, args) {
-        message.channel.send('crikey! thats a nice external command you got there toad!');
-    },
-    
-};
+// export
