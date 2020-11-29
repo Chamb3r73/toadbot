73ad9a0f51e697a78a5fa69ede4b5a86127ab969
Chamb3r73 <59922982+Chamb3r73@users.noreply.github.com>
2020-11-29 19:00:28 +0000

    the sex

--- bot.js
+++ bot.js
@@ -23,7 +23,7 @@ client.once('ready', () => {
 
 client.on('message', async message => 
 	
-	{if (!message.content.startsWith(prefix) || message.author.bot) return;
+	{if (!message.content.startsWith(prefix) return;
 
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
 	const command = args.shift().toLowerCase();
