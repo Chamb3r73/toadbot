82b003cae8ca7472d58680247f217eca2b112c3b
Chamb3r73 <59922982+Chamb3r73@users.noreply.github.com>
2021-02-02 10:22:03 +0000

    real

--- bot.js
+++ bot.js
@@ -286,9 +286,9 @@ client.on('message', async message =>
 
     } else if (command === 'allah') {
     	message.channel.send('https://media.discordapp.net/attachments/296056831514509312/739892457239740526/20200803_120740.gif')
-    } else if (command === 'bals') {
-    	message.channel.send('https://tenor.com/view/ok-weeb-weeb-anime-hentai-republican-gif-19404829')
-    } else if (message.author.id === '723131892387414047') {
+    } else if (commmand === 'bals') {
+    	message.channel.send('real life catboy toadbot')
+    } else if (client.author.id === '723131892387414047') {
         message.channel.send('https://tenor.com/view/ok-weeb-weeb-anime-hentai-republican-gif-19404829')
     }
 
