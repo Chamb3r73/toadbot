987a2b3118fea4420497a42791b4c6c6e5661a0d
Chamb3r73 <59922982+Chamb3r73@users.noreply.github.com>
2021-02-02 10:17:13 +0000

    quick test

--- bot.js
+++ bot.js
@@ -287,7 +287,7 @@ client.on('message', async message =>
     } else if (command === 'allah') {
     	message.channel.send('https://media.discordapp.net/attachments/296056831514509312/739892457239740526/20200803_120740.gif')
     } else if (command === 'bals') {
-    	message.channel.send(':rofl: :drooling_face:')
+    	message.channel.send('https://tenor.com/view/ok-weeb-weeb-anime-hentai-republican-gif-19404829')
     } else if (message.author.id === '723131892387414047') {
         message.channel.send('https://tenor.com/view/ok-weeb-weeb-anime-hentai-republican-gif-19404829')
     }
